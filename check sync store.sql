cbd42f129c2ded2c86e0c0e8120635803ef744b9
Vo Minh Son <minhson.uit@gmail.com>
2022-03-30 15:32:58 +0700

    Add files via upload
    check data upload from store to server by date

---
+++ check sync store.sql
@@ -0,0 +1,17 @@
+declare @startdate date = '2022-03-24'
+
+with
+  dates as (
+    select @startdate as [date]
+    union all
+    select dateadd(dd, 1, [date]) from dates where [date] < '2022-03-30'
+  )
+  select a.OPENDATE, b.date,a.STOREID,count(c.recid),sum(grossamount)/1000000.0 from PMCAPPSTOREEXT A
+  left join dates B
+  on a.PARTITION=5637144576 and a.DATAAREAID='phct'
+  left join RETAILTRANSACTIONTABLE C
+  on c.STORE = a.STOREID and c.TRANSDATE = b.date  and C.PARTITION=5637144576 and C.DATAAREAID='phct'
+  where a.STORESTATUS=1
+  and a.OPENDATE>=[date]
+  group by STOREID,[date],a.OPENDATE
+  order by date desc
