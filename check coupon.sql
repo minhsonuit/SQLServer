c91ebaba81d1b2590549e9d13ff12779bac763f0
Vo Minh Son <minhson.uit@gmail.com>
2022-04-11 11:08:30 +0700

    Create check coupon.sql

---
+++ check coupon.sql
@@ -0,0 +1,16 @@
+select
+X.STORENUMBER,count(x.recid) as total
+ from
+(
+select a.STORENUMBER,b.RECID from RETAILCHANNELTABLE A
+left join retailGiftCardTable B
+on a.recid % 40 = b.recid % 40
+and b.PMCGIFTCARDPROGRAMID='GC-001052'
+and a.PARTITION=5637144576 
+and b.PARTITION=5637144576 and b.DATAAREAID='phct'
+and b.PMCGIFTCARDSTATUS=0
+) X
+
+group by x.STORENUMBER
+--having count(x.recid)<=0
+order by x.STORENUMBER
