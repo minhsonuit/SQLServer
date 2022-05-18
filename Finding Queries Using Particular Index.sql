93b846f6ab3e77729709384a9517709ddeb76fc5
Vo Minh Son <minhson.uit@gmail.com>
2022-05-18 14:24:20 +0700

    Create Finding Queries Using Particular Index.sql

---
+++ Finding Queries Using Particular Index.sql
@@ -0,0 +1,25 @@
+SELECT 
+SUBSTRING(sqltext.text, (deqs.statement_start_offset / 2) + 1,
+(CASE deqs.statement_end_offset
+WHEN -1 THEN DATALENGTH(sqltext.text)
+ELSE deqs.statement_end_offset
+END - deqs.statement_start_offset) / 2 + 1) AS sqltext,
+deqs.execution_count,
+deqs.total_logical_reads/execution_count AS avg_logical_reads,
+deqs.total_logical_writes/execution_count AS avg_logical_writes,
+deqs.total_worker_time/execution_count AS avg_cpu_time,
+deqs.last_elapsed_time/execution_count AS avg_elapsed_time,
+deqs.total_rows/execution_count AS avg_rows,
+deqs.creation_time,
+deqs.last_execution_time,
+CAST(query_plan AS xml) as plan_xml
+FROM sys.dm_exec_query_stats as deqs
+CROSS APPLY sys.dm_exec_text_query_plan
+(deqs.plan_handle, deqs.statement_start_offset, deqs.statement_end_offset)
+as detqp
+CROSS APPLY sys.dm_exec_sql_text(deqs.sql_handle) AS sqltext
+WHERE
+--detqp.query_plan like '%custtable%'
+ORDER BY deqs.last_execution_time DESC
+OPTION (MAXDOP 1, RECOMPILE);
+---ref https://blog.sqlauthority.com/2021/07/15/sql-server-finding-queries-using-particular-index/
